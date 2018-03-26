6b12c4cb5c93798366c5d78f512335716106c02a
Steven <xhrsteven@gmail.com>
2018-03-25 23:18:10 -0500

    updated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,53 +1,61 @@
+//Function process
 function start() {
-    document.getElementById('quiz').style.visibility='visible';
+    document.getElementById('quizForm').style.visibility='visible';
 }
+var correct = 0;
+var wrong = 0;
 function check() {
-    var question1 = document.quiz.movie.value;
-    var question2 = document.quiz.girl.value;
-    var question3 = document.quiz.team.value;
-    var question4 = document.quiz.song.value;
-    var question5 = document.quiz.songMovie.value;
-    var question6 = document.quiz.cab.value;
-    var question7 = document.quiz.friend.value;
-    var question8 = document.quiz.principle.value;
-    var correct = 0;
     
-    if(question1 ==='Toy Story'){
-        correct++;
-    }
-    if(question2 ==='Fried Spicy'){
-        correct++;
-    }
-    if(question3 ==='Chicago Bulls') {
-        correct++;
-    }
-    if (question4 === "Nirvana") {
-        correct++;
-    }
-    if (question5 === "The Lion King"){
-        correct++;
-    }
-    if (question6 === "Fresh") {
-        correct++;
-    }
-    if (question7 === "Skeeter") {
-      correct++;
-    }
-    if (question8 === "Mr.Belding") {
-      correct++;
-    }
-     var message = ["Good Job", "That's just okay", "You really need to do better"];
-
-    var range;
-    if(correct<3){
-        range =2;
-    }else if(correct>2 && correct<6){
-        range =1;
-    }else{
-        range =0;
-    }
-    var html = '<p>'+message[range] +'</p>'+
-    '<h1>You got'+correct+'correct.'
-    document.querySelector('#quiz-game').innerHTML = html;
-    document.getElementById('#quiz').style.visibility = 'hidden';
+    // var name = document.querySelectorAll('input[id]:checked');
+    // for(var i=0; i<=name.length;i++){
+    //     console.log(name[i].id);
+    //     if(name[i].id =='true'){
+    //         console.log("correct: "+ correct++);
+    //     }else if(name[i].id =='wrong'){
+    //         console.log("wrong: "+ wrong++);
+    //     }
+    // }
+   
 }
+var range = 0;
+var keyWord = ['excellent', 'meh', 'tryNextTime'];
+var message = ["Good Job", "That's just okay", "You really need to do better"];
+function result() {
+    document.getElementById('quizForm').style.visibility = 'hidden';
+    var pic = $('<img>');
+    var srcRoute= [, , ];
+    if (correct < 3) {
+        range = 2;
+        pic.attr('src', 'images/fail.png');
+        $('#game-start').prepend(pic);
+    } else if (correct > 2 && correct < 6) {
+        range = 1;
+        pic.attr('src', 'images/meh.gif')
+    } else {
+        range = 0;
+        pic.attr('src','images/success.gif');
+    }   
+    
+   
+    pic.attr('alt',keyWord[range]);
+    var html = $('#game-start');
+    html.append(`<h1>${message[range]}</h1>`);
+    html.append(pic);
+    html.append(`<h2>You got ${this.correct} correct.<h2>`);
+}
+   
+
+//Main process
+$("#game-start").on("click",function () {
+    $('#game-start').empty();
+    var quiz = document.getElementsByClassName('quizForm');
+    start();
+    $('#game-start').append(quiz);
+
+})
+//Submit Button
+$('#submit').on('click',function () {
+    $('#game-start').empty();
+    check();
+    result();
+})
